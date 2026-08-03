f412497155487e20882737957fb09096a7c46ee6
khushichaudhary01232-rgb <khushichaudhary01232@gmail.com>
2026-08-03 13:51:30 +0530

    introduction to js

---
+++ Demo2.js
@@ -0,0 +1,22 @@
+/*console.log("console")
+console.info("info")
+console.warn("warn")
+console.error("err")
+console.assert("err"!=false)
+console.assert("err"==false)
+//alert("your script works")
+
+let a = prompt("enter a here")
+document.write(a)
+console.log(typeof (a))
+let num= Number.parseInt(a)
+console.log(typeof(n))
+
+console.log(window)
+document.body.style.background="red"
+location.href ="https://codewithharry.com"
+*/
+//pracise set 6 
+
+
+
