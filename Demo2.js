bfbc9d29b1e3aa12696ef1df397e3cbfd279ce25
khushichaudhary01232-rgb <khushichaudhary01232@gmail.com>
2026-08-17 13:46:42 +0530

    merning new concepts of js

--- Demo2.js
+++ Demo2.js
@@ -16,7 +16,74 @@ console.log(window)
 document.body.style.background="red"
 location.href ="https://codewithharry.com"
 */
+/*
 //pracise set 6 
+let runAgain=true
+while (runAgain){
+    let r = prompt("enter your age")
+    r=Number.parseInt(r)
+    if(r>18){
+    alert("yoy can drive")
+    }
+    if (r<0){
+        console.error("age cannot be neg")
+        break;
+    }
+    else{
+    alert("yoy cannot drive")
+    }
+    runAgain=confirm("want to rum again")
+}
+let color= prompt("enter the color")
+console.log(window)
+document.body.style.background=color 
+
+let num = prompt("enter the number")
+num=Number.parseInt(num)
+if (num>4){
+    location.href="https://google.com"
+}*/
+/*
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+let arr= Array.from(document.body.childNodes)
+console.log(arr)
+
+console.log(document.body.firstChild)
+a=document.body.firstElementChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
+*/
+/*
+console.log (document.body.firstChild)
+console.log(document.body.firstElementChild)
+
+let t=document.body.firstElementChild.firstElementChild
+console.log(t.tHead)
+console.log(t.tBody)
+console.log(t.context)
+
+let title = document.getElementById("num")
+title.style.color="red"
+let titles = document.querySelectorAll("#num")
+titles.style.color=("yellow")
+*/
+let id = document.getElementById("id1")
+let sp1 = document.getElementById("sp1")
+console.log (id1)
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+console.log(id1.closest(".box"))
+console.log(sp1.closest("#sp1"))
+console.log(sp1.contains(id1))
+console.log(id1.contains(sp1))
+
+
+
+
+
 
 
 
